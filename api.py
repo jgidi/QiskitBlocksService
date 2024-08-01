8785fdcb48a4ce3dfa19812614fcd38fde7df21a
Jorge Gidi <jorgegidi@gmail.com>
2024-08-01 04:33:42 +0200

    update api to qiskit 1.0

--- api.py
+++ api.py
@@ -14,29 +14,31 @@
 # limitations under the License.
 #
 from qiskit import QuantumRegister, ClassicalRegister
-from qiskit import QuantumCircuit, BasicAer, execute
+from qiskit import QuantumCircuit
+from qiskit_aer import Aer
 
 
 def run_qasm(qasm, backend_to_run='qasm_simulator', num_shots_str='1'):
     circuit = QuantumCircuit.from_qasm_str(qasm)
-    backend = BasicAer.get_backend(backend_to_run)
-    job_sim = execute(circuit, backend, shots=int(num_shots_str))
+    backend = Aer.get_backend(backend_to_run)
+    job_sim = backend.run(circuit, shots=int(num_shots_str))
     result_sim = job_sim.result()
     return result_sim.get_counts(circuit)
 
 
 def get_statevector(qasm, backend_to_run='statevector_simulator'):
     circuit = QuantumCircuit.from_qasm_str(qasm)
-    backend = BasicAer.get_backend(backend_to_run)
-    job_sim = execute(circuit, backend)
+    backend = Aer.get_backend(backend_to_run)
+    job_sim = backend.run(circuit)
     result_sim = job_sim.result()
     output_state = result_sim.get_statevector(circuit, decimals=3)
+    print(output_state)
     return output_state
 
 def get_unitary(qasm, backend_to_run='unitary_simulator'):
     circuit = QuantumCircuit.from_qasm_str(qasm)
-    backend = BasicAer.get_backend(backend_to_run)
-    job_sim = execute(circuit, backend)
+    backend = Aer.get_backend(backend_to_run)
+    job_sim = backend.run(circuit)
     result_sim = job_sim.result()
     output_state = result_sim.get_unitary(circuit, decimals=3)
     return output_state
